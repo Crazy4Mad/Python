1307e41c406021c9d6b48983027278f79c77d936
sheo18u865 <gshevts@mail.ru>
2019-04-24 16:20:27 +0300

    Корректная работа для отрезка без корней

--- lab_03/scratch_7.py
+++ lab_03/scratch_7.py
@@ -7,7 +7,7 @@ def new_x_find(prev, prev_prev):
     return prev - ((prev - prev_prev) / (function(prev) - function(prev_prev))*function(prev))
 
 def find_root(from_x, to_x, bias, max_iters):
-    if to_x - from_x >= 2*pi:
+    if abs(to_x - from_x) >= 2*pi:
         return "More than one root"
     cur_x, prev_x, prev_prev = new_x_find(from_x, to_x), from_x, to_x
     iters = 1
@@ -17,14 +17,12 @@ def find_root(from_x, to_x, bias, max_iters):
         cur_x = new_x_find(prev_x, prev_prev)
         iters += 1
     if cur_x + pi <= to_x or cur_x - pi >= from_x:
-        flag = False
+        cur_x = 'No roots'
     if flag:
         if iters > max_iters:
             return "Max amount of operations exceeded"
-        elif cur_x > to_x or cur_x < from_x:
-            return "No roots"
         else:
             return [cur_x, iters]
     else:
         return "More than one root"
-print(find_root(-3, -1, 0.001, 9))
+print(find_root(-pi/2-1, -1, 0.001, 9))
